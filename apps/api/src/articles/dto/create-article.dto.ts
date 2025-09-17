6a72dd24157ef2eae74e1fc4a8f2da3e44e96185
송재진 <wowlxx28@gmail.com>
2025-09-17 23:06:42 +0900

    feat: class-validator, class-transformer 적용

--- apps/api/src/articles/dto/create-article.dto.ts
+++ apps/api/src/articles/dto/create-article.dto.ts
@@ -1,10 +1,39 @@
+import {
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  ArrayNotEmpty,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 import { Prisma } from '@prisma/client';
 
 export class CreateArticleDto implements Prisma.ArticleCreateInput {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsUrl()
+  @IsNotEmpty()
   url: string;
+
+  @IsString()
+  @IsNotEmpty()
   summary: string;
+
+  @IsString()
+  @IsNotEmpty()
   source: string;
-  published_at: string | Date | null;
+
+  @IsOptional()
+  @Type(() => Date) // 문자열 → Date 변환
+  @IsDate()
+  published_at: Date | null;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
   tagNames: string[];
 }
